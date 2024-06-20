d10f3228bf51414e952373cd53793e835bcddfeb
Victoria-2 <ma.victoria.2509@gmail.com>
2024-06-20 17:17:37 -0300

    Definicion de clases

---
+++ src/AVLTree.java
@@ -0,0 +1,12 @@
+public class AVLTree {
+    Nodo raiz;
+
+    Nodo insertar(Nodo nodo, int valor){
+        if(nodo == null){
+            return new Nodo(valor);
+        }
+
+        return nodo;
+    }
+
+}
