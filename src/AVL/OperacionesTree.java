5bcc3fa93dfc2feb1aea598781b3eb6bb6cf1c71
ValenAbarzua <valefer119@gmail.com>
2024-06-28 10:47:30 -0300

    Agrege algunos metodos a OperacionesTree.java, aunque nose si funcionaran. El AVLTree.java tiene lo mismo que operaciones, nose si queres sacarlo o pensabas agregar algo mas a esa clase!!, pero lo vamos viendo

--- src/AVL/OperacionesTree.java
+++ src/AVL/OperacionesTree.java
@@ -1,4 +1,32 @@
 package AVL;
 
 public class OperacionesTree {
+    public Nodo raiz;
+
+    public OperacionesTree(){
+        this.raiz= null;
+    }
+
+    public void InsertarNodo(char valor){ //Inserta un nodo en el arbol
+        this.raiz = insertarRecursivo(this.raiz, valor);
+    }
+
+    public Nodo insertarRecursivo(Nodo nodo, char valor){
+        if (nodo == null){
+            return new Nodo(valor);
+        }
+       return nodo;
+    }
+
+    public int evaluarExpresion(){ //Proceso para evaluar la expresion aritmetica del arbol
+        return evaluarRecursivo(this.raiz);
+    }
+
+    private int evaluarRecursivo(Nodo nodo){ //Recorre y evalua en recursividad
+        if (nodo==null){
+            return 0;
+        }
+        return 0; //Valor de retorno temporal
+    }
+
 }
